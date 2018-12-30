dd204f2043a0bb1f41472da91b461b4b0869ebcc
Jiyeon Seo <jiyeonseo@fb.com>
2018-12-31 01:16:51 +0900

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=cheesecheeseball-Web-Deploy.pubxml -p:Password=YABY9szvl9p908L1fi95CzjEyXtuHZliS9YwhS5wu7r2EBw4gAJmsrspY0dl
+
